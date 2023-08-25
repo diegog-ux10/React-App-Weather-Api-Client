1c6d7afb3bd7d786a023157bff09fbf0403c37cd
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-08-25 15:23:34 -0400

    Created api client with axios

--- src/api/weather-api.ts
+++ src/api/weather-api.ts
@@ -1,9 +1,32 @@
 import { instance } from "./base.api";
 
-const endPoint = "/forecast";
+const endPoint = "/weather";
 
 export const weatherApi = {
-    getWeather: () => {
-        return instance.get(endPoint, {params: {id: `524901`, appid: "44f1b0b0e9952ff9fbe6a475ec8b5f1a"}})
-    }
+  getWeather: (city: string) => {
+    return instance.get(endPoint, {
+      params: {
+        q: city,
+        units: "metric",
+        appid: "44f1b0b0e9952ff9fbe6a475ec8b5f1a",
+      },
+    });
+  },
+
+  getWeatherLatLon: (lat: string, lon: string) => {
+    return instance.get(endPoint, {
+      params: {
+        lat: lat,
+        lon: lon,
+        units: "metric",
+        appid: "44f1b0b0e9952ff9fbe6a475ec8b5f1a",
+      },
+    });
+  },
+
+  getForecast: (lat: string, lon: string) => {
+    return instance.get("onecall", {
+      params: { lat, lon, appid: "44f1b0b0e9952ff9fbe6a475ec8b5f1a", cnt: 5, units: "metric", },
+    });
+  },
 };
