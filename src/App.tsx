40678ee2598e39ce290b6bbce6e51ca55211aa29
diegogranados143@gmail.com <diego@codeando.edu.pe>
2023-08-25 15:28:18 -0400

    refactor: added sidebar and forecastList componentes to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
+import { useContext } from "react";
 
-import { Box } from '@mui/material'
-import './App.css'
-import { Search } from './components/search/search'
+import "./App.css";
 
+import { SideBar } from "./components/SideBar";
+
+import { WeatherContext, WeatherContextType } from "./context/WeatherContext";
+import { Box } from "@mui/material";
+import { theme } from "./theme/theme";
+import { ForecastList } from "./components/ForecastList";
 
 function App() {
-
-  const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
-    
-  }
-  
+  const { currentCity } = useContext(WeatherContext) as WeatherContextType;
 
   return (
     <>
-       <Box display="flex">
-        <Box>
-          <Search onSearchChange={handleOnSearchChange} />
+      <Box display="flex" sx={{ width: "100vw", height: "100vh" }}>
+        <Box sx={{ width: "30%" }}>{currentCity && <SideBar />}</Box>
+        <Box
+          sx={{
+            width: "70%",
+            height: "100%",
+            backgroundColor: theme.palette.primary.main,
+            padding: "80px"
+          }}
+        >
+          <Box>
+            <ForecastList />
+          </Box>
+          <Box>
+            <h1>Todays HighLights</h1>
+          </Box>
         </Box>
-        <Box>
-          
-        </Box>
-       </Box>
+      </Box>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
